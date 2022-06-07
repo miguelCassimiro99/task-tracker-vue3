779ec2bc9aa54dc5c0d6cc21d76e74d61e9fba67
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2022-06-07 08:31:01 -0300

    filtering the tasks by the exact name

--- src/views/TarefasLista.vue
+++ src/views/TarefasLista.vue
@@ -1,29 +1,30 @@
 <template lang="pug">
 Formulario(@aoSalvarTarefa="salvarTarefa")
 .lista
+  section.mb-3
+      .field
+        p.control.has-icons-left.has-icons-right
+          input.input(type="text" placeholder="filtrar tarefas" v-model="filtro")
+          span.icon.is-small.is-left
+            i.fas.fa-search
+          span.icon.is-small.is-right
+            i.fas.fa-check
+  
   BoxComponent.is-flex.is-justify-content-start.is-align-items-center(
     v-if="!tarefas || tarefas.length === 0"
   )
     h1 Você não realizou nenhuma tarefa hoje
     i.ml-2.fas.fa-face-frown
-  
-  section(v-else)
-    .field
-      p.control.has-icons-left.has-icons-right
-        input.input(type="text" placeholder="filtrar tarefas" v-model="filtro")
-        span.icon.is-small.is-left
-          i.fas.fa-search
-        span.icon.is-small.is-right
-          i.fas.fa-check
 
-    TarefaComponent(
-      @aoTarefaClicada="selecionarTarefa"
-      v-for="(tarefa, index) in tarefas"
-      :tarefa="tarefa"
-      :key="index"
-      :descricao="tarefa.descricao"
-      :duracao="tarefa.duracaoEmSegundos"
-    )
+  TarefaComponent(
+    v-else
+    @aoTarefaClicada="selecionarTarefa"
+    v-for="(tarefa, index) in tarefas"
+    :tarefa="tarefa"
+    :key="index"
+    :descricao="tarefa.descricao"
+    :duracao="tarefa.duracaoEmSegundos"
+  )
 
   //-Modal
   .modal(:class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada")
@@ -104,14 +105,8 @@ export default defineComponent({
 
     const filtro = ref("")
 
-    // const tarefas = computed(() =>
-    //   store.state.tarefa.tarefas.filter(
-    //     (t) => !filtro.value || t.descricao.includes(filtro.value)
-    //   )
-    // )
-
     watchEffect(() => {
-      console.log(filtro)
+      store.dispatch(OBTER_TAREFAS, filtro.value)
     })
 
     return {
